c0775a08f8e3d666b1c309a7e1facc6a5bae906a
Aeronec <romain.coguic@outlook.fr>
2026-04-04 19:05:39 +0200

    Ajout de la DA sur le bouton fin de tour, faudra peut être changer la police et la coleur car je les trouve pas tip top

--- Scripts/in_game/MAP/MapManager.gd
+++ Scripts/in_game/MAP/MapManager.gd
@@ -96,7 +96,7 @@ func spawn_tile_object(cell: HexCell):
 	var offset_coords = cell.getTabCoordinates()
 	var pixel_pos = Map_utils.hex_to_pixel_iso(offset_coords.x, offset_coords.y)
 	s.position = pixel_pos
-	
+				
 	add_child(s)
 	
 	if cell.getTypeTerrain() == "port":
@@ -106,6 +106,8 @@ func spawn_tile_object(cell: HexCell):
 		add_child(port_node)
 		
 		cell.port_instance = port_node
+		
+		
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:

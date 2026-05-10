4a3cc9758be94f56fa4d2443cb26a2005c8f07bb
Aeronec <romain.coguic@outlook.fr>
2026-05-10 12:23:18 +0200

    Nouvelle Texture, si y'en qui se plaint des nouvelles qu'il aille bien se faire enculé sale fdp

--- Scripts/in_game/MAP/MapManager.gd
+++ Scripts/in_game/MAP/MapManager.gd
@@ -89,12 +89,8 @@ func spawn_tile_object(cell: HexCell):
 	if(s.texture == Map_data.TileMissing):
 		DEBUG.log("Texture manquante pour le terrain '%s'" % cell.getTypeTerrain(),DEBUG.WARNING)
 	
-	var scale_x = Map_data.hex_width / float(Map_data.TILE_WIDTH)
-	var scale_y = Map_data.hex_height / float(Map_data.TILE_HEIGHT)
-
-	if cell.getTypeTerrain() == "fish":
-		scale_x = Map_data.hex_width / float(s.texture.get_width())
-		scale_y = Map_data.hex_height / float(s.texture.get_height())
+	var scale_x = Map_data.hex_width / float(s.texture.get_width())
+	var scale_y = Map_data.hex_height / float(s.texture.get_height())
 	s.scale = Vector2(scale_x, scale_y)
 
 	# Utilisation des coordonnées offset stockées dans la cellule
